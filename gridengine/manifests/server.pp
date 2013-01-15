ce2a68dbebb31e1f92f1776d4811627eb004c872
emir imamagic <emir.imamagic@gmail.com^>
2013-01-15 06:35:08 +0100

    Added complete SGE module

--- gridengine/manifests/server.pp
+++ gridengine/manifests/server.pp
@@ -1,9 +1,19 @@
-# Class: gridengine::node
+# Class: gridengine::server
 #
 # This class installs the SGE master
 #
 class gridengine::server inherits gridengine {
   package { 'gridengine-qmaster':
     ensure => present,
+  }
+  package { 'gridengine-qmon':
+    ensure => present,
+  }
+  file { '/opt/sge/util/install_modules/inst_common.sh':
+    require => Package['gridengine'],
+    source  => 'puppet:///modules/gridengine/qmaster-inst_common.sh',
+    owner   => root,
+    group   => root,
+    mode    => '0755',
   }
 }
