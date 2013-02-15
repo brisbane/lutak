73d8e28f301968f80b71785c088528e3ec9895c9
Jakov Sosic <jsosic@gmail.com>
2013-02-15 19:05:42 +0100

    added zabbix20 (next generation) module

---
+++ zabbix20/manifests/agent.pp
@@ -0,0 +1,32 @@
+# Class: zabbix20::agent
+#
+# This module manages zabbix-agent
+#
+class zabbix20::agent (
+  $package_ensure = $zabbix20::package_ensure,
+  $major          = $zabbix20::major,
+  $server_name    = $zabbix20::server_name,
+  $server_active  = $zabbix20::server_active,
+  $client_name    = $zabbix20::client_name,
+) inherits zabbix20 {
+
+  package { 'zabbix-agent':
+    ensure   => $package_ensure,
+    name     => "zabbix${major}-agent",
+  }
+  service { 'zabbix-agent':
+    ensure   => running,
+    enable   => true,
+    provider => redhat,
+    require  => Package['zabbix-agent'],
+  }
+  file { '/etc/zabbix_agentd.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('zabbix20/zabbix_agentd.conf.erb'),
+    require => Package['zabbix-agent'],
+    notify  => Service['zabbix-agent'],
+  }
+}
