2096a6ba81c118e24285fb108bc9c293ec94fd11
emir imamagic <emir.imamagic@gmail.com>
2013-02-18 12:09:11 +0100

    Added gfarm module. Added patch for Globus GridFTP. Removed hack for Globus Gatekeeper + grid_monitor. Added new link for OSCAR repo. Corrected UMD repo definition.

--- globus/manifests/gatekeeper.pp
+++ globus/manifests/gatekeeper.pp
@@ -16,44 +16,44 @@ class globus::gatekeeper (
     ensure => present,
   }
   file { '/etc/sysconfig/globus-gatekeeper':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['globus-gram-job-manager-fork-setup-poll'],
-    content  => template('globus/globus-gatekeeper.erb'),
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0644',
+    require   => Package['globus-gram-job-manager-fork-setup-poll'],
+    content   => template('globus/globus-gatekeeper.erb'),
   }
   service { 'globus-gatekeeper':
-    enable    => true,
     ensure    => running,
+    enable    => true,
     provider  => redhat,
     require   => File['/etc/sysconfig/globus-gatekeeper'],
   }
 
   # temporary hack needed for Condor grid-monitor script
-  file { ['/usr/lib/perl', '/usr/lib/perl/Globus', '/usr/lib/perl/Globus/GRAM']:
-    ensure    => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  file { '/usr/lib/perl/Globus/GRAM/JobManager':
-    ensure  => link,
-    target  => '/usr/lib/perl5/vendor_perl/5.8.8/Globus/GRAM/JobManager',
-    require => File['/usr/lib/perl/Globus/GRAM'],
-  }
-  file { '/usr/etc/grid-services':
-    ensure  => link,
-    target  => '/etc/grid-services',
-  }
-  file { ['/usr/var', '/usr/var/tmp']:
-    ensure    => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  file { '/usr/var/tmp/gram_job_state':
-    ensure  => link,
-    target  => '/var/lib/globus/gram_job_state',
-    require => File['/usr/var/tmp'],
-  }
+  #file { ['/usr/lib/perl', '/usr/lib/perl/Globus', '/usr/lib/perl/Globus/GRAM']:
+  #  ensure    => directory,
+  #  owner  => root,
+  #  group  => root,
+  #  mode   => '0755',
+  #}
+  #file { '/usr/lib/perl/Globus/GRAM/JobManager':
+  #  ensure  => link,
+  #  target  => '/usr/lib/perl5/vendor_perl/5.8.8/Globus/GRAM/JobManager',
+  #  require => File['/usr/lib/perl/Globus/GRAM'],
+  #}
+  #file { '/usr/etc/grid-services':
+  #  ensure  => link,
+  #  target  => '/etc/grid-services',
+  #}
+  #file { ['/usr/var', '/usr/var/tmp']:
+  #  ensure    => directory,
+  #  owner  => root,
+  #  group  => root,
+  #  mode   => '0755',
+  #}
+  #file { '/usr/var/tmp/gram_job_state':
+  #  ensure  => link,
+  #  target  => '/var/lib/globus/gram_job_state',
+  #  require => File['/usr/var/tmp'],
+  #}
 }
