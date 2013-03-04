7fcfa938beedb803b981200a2b6750af18f2243c
Luko Gjenero <lgjenero@gmail.com>
2013-03-04 12:01:58 +0100

    TSM module - added control for archive and backup service

--- tsm/manifests/init.pp
+++ tsm/manifests/init.pp
@@ -3,8 +3,10 @@
 # This module manages tsm
 #
 class tsm (
-  $package_ensure = $tsm::params::package_ensure,
-  $exclude        = $tsm::params::exclude,
+  $package_ensure  = $tsm::params::package_ensure,
+  $exclude         = $tsm::params::exclude,
+  $backup_service  = $tsm::params::backup_service,
+  $archive_service = $tsm::params::archive_service,
 ) inherits tsm::params {
   include yum::repo::srce::intern
 
@@ -17,7 +19,42 @@ class tsm (
     provider => redhat,
     require  => Package['tsm-client'],
   }
+
+
+  if $backup_service == 'running' {
+    service { [ 'dsmcad-backup']:
+      ensure   => running,
+      enable   => true,
+      provider => redhat,
+      require  => Package['tsm-client'],
+    }
+  }
+  else {
+    service { [ 'dsmcad-backup']:
+      ensure   => 'stopped',
+      enable   => false,
+      provider => redhat,
+      require  => Package['tsm-client'],
+    }
+  }
+
+  if $archive_service == 'running' {
+    service { [ 'dsmcad-archive']:
+      ensure   => running,
+      enable   => true,
+      provider => redhat,
+      require  => Package['tsm-client'],
+    }
+  }
+  else {
+    service { [ 'dsmcad-archive']:
+      ensure   => 'stopped',
+      enable   => false,
+      provider => redhat,
+      require  => Package['tsm-client'],
+    }
+  }
+
   # TODO:
   # expect -c "spawn /usr/bin/dsmc expect">:" send \"\r\" expect ":" send \"sunce.srce+tr00ba\r\" "
-
 }
