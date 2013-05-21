048b984251571b10e89b0015d63713302397af72
Jakov Sosic <jsosic@gmail.com>
2013-05-21 18:00:07 +0200

    added CentOS 6.4 to Cobbler, revamp of mcollective (now depends on puppetlabs/rabbitmq)

--- mcollective/manifests/mqserver.pp
+++ mcollective/manifests/mqserver.pp
@@ -14,39 +14,10 @@
 class mcollective::mqserver (
   $package_ensure = $mcollective::params::package_ensure,
 ) inherits mcollective::params {
-  package { 'rabbitmq-server':
-    ensure  => $package_ensure,
-  }
-  # get version of plugins depending on RabbitMQ version
-  file { "/usr/lib/rabbitmq/lib/rabbitmq_server-${::rabbitmqversion}/plugins/amqp_client-${::rabbitmqversion}.ez":
+  include ::rabbitmq::server
+
+  rabbitmq_plugin {'rabbitmq_stomp':
     ensure   => present,
-    source   => "puppet:///modules/mcollective/rabbitmq/amqp_client-${::rabbitmqversion}.ez",
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    require  => Package['rabbitmq-server'],
-  }
-  file { "/usr/lib/rabbitmq/lib/rabbitmq_server-${::rabbitmqversion}/plugins/rabbitmq_stomp-${::rabbitmqversion}.ez":
-    ensure   => present,
-    source   => "puppet:///modules/mcollective/rabbitmq/rabbitmq_stomp-${::rabbitmqversion}.ez",
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    require  => Package['rabbitmq-server'],
-  }
-  file { '/etc/rabbitmq/rabbitmq-env.conf':
-    ensure   => present,
-    content  => template('mcollective/rabbitmq-env.conf.erb'),
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    require  => [ Package['rabbitmq-server'], File["/usr/lib/rabbitmq/lib/rabbitmq_server-${::rabbitmqversion}/plugins/amqp_client-${::rabbitmqversion}.ez"], File["/usr/lib/rabbitmq/lib/rabbitmq_server-${::rabbitmqversion}/plugins/rabbitmq_stomp-${::rabbitmqversion}.ez"], ],
-    notify   => Service['rabbitmq-server'],
-  }
-  service { 'rabbitmq-server':
-    ensure   => 'running',
-    enable   => true,
-    provider => 'redhat',
-    require  => File['/etc/rabbitmq/rabbitmq-env.conf'],
+    provider => 'rabbitmqplugins',
   }
 }
