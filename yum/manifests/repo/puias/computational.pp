19790904b2404bcb9228a5c100f40b5e6e49c40c
Jakov Sosic <jsosic@gmail.com>
2013-08-15 19:22:14 +0200

    added PUIAS repo as dep of Russian

---
+++ yum/manifests/repo/puias/computational.pp
@@ -0,0 +1,27 @@
+# Class: yum::repo::puias::computational
+#
+# This module manages Puias computational repo files for $operatingsystemrelease
+#
+
+# CentOS
+class yum::repo::puias::computational (
+  $stage    = 'yumsetup',
+  $priority = '99',
+  $exclude  = '',
+){
+  require yum::repo::puias::addons
+
+  case $::operatingsystem {
+    default : {}
+    'CentOS' : {
+      file { '/etc/yum.repos.d/puias-computational.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/puias-computational.erb"),
+      }
+    }
+  }
+
+}
