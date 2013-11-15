fcd41f233522d673acb1e79e70f89113522eb5be
Jakov Sosic <jsosic@gmail.com>
2013-11-15 17:05:08 +0100

    add support for vmware tools installation

--- vmtools/manifests/init.pp
+++ vmtools/manifests/init.pp
@@ -3,7 +3,8 @@
 # Installs the Vm tools, depending on type of virtualization
 class vmtools {
   case $::virtual {
-    default: {}
-    /^xen*/: { include ::vmtools::xen }
+    default:    {}
+    /^xen*/:    { include ::vmtools::xen }
+    /^vmware$/: { include ::vmwaretools  }
   }
 }
