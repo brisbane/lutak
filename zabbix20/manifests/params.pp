800cacdc64574736218cc626696e96765a1336d0
Emir Imamagic <emir.imamagic@gmail.com>
2013-12-04 11:34:52 +0100

    Zabbix 20 improvements. Added zabbix20::server::config for setting server config variables.

--- zabbix20/manifests/params.pp
+++ zabbix20/manifests/params.pp
@@ -3,14 +3,17 @@
 # This module contains defaults for zabbix modules
 #
 class zabbix20::params {
-  $package_ensure = 'present'
-  $major          = ''
-  $server_name    = 'mon'
-  $server_active  = 'mon'
-  $client_name    = $::fqdn
-  $db             = 'pgsql'
-  $dbhost         = 'localhost'
-  $dbname         = 'zabbix'
-  $dbuser         = 'zabbix'
-  $dbpass         = 'secret'
+  $package_ensure            = 'present'
+  $major                     = ''
+  $server_name               = 'mon'
+  $server_active             = 'mon'
+  $client_name               = $::fqdn
+  $db                        = 'pgsql'
+  $dbhost                    = 'localhost'
+  $dbname                    = 'zabbix'
+  $dbuser                    = 'zabbix'
+  $dbpass                    = 'secret'
+  $dbsocket                  = false
+  $dbsocket_path             = '/var/lib/mysql/mysql.sock'
+  $purge_conf_dir            = false
 }
--- zabbix20/manifests/server.pp
+++ zabbix20/manifests/server.pp
@@ -3,14 +3,19 @@
 # This module manages zabbix-server
 #
 class zabbix20::server (
-  $package_ensure = $zabbix20::package_ensure,
-  $major          = $zabbix20::major,
-  $listenip       = '0.0.0.0',
-  $db             = $zabbix20::db,
-  $dbhost         = $zabbix20::dbhost,
-  $dbname         = $zabbix20::dbname,
-  $dbuser         = $zabbix20::dbuser,
-  $dbpass         = $zabbix20::dbpass,
+  $package_ensure            = $zabbix20::package_ensure,
+  $major                     = $zabbix20::major,
+  $listenip                  = '0.0.0.0',
+  $db                        = $zabbix20::db,
+  $dbhost                    = $zabbix20::dbhost,
+  $dbname                    = $zabbix20::dbname,
+  $dbuser                    = $zabbix20::dbuser,
+  $dbpass                    = $zabbix20::dbpass,
+  $dbsocket                  = $zabbix20::dbsocket,
+  $dbsocket_path             = $zabbix20::dbsocket_path,
+  $alert_scripts_path        = '/etc/zabbix/alert.d',
+  $external_scripts          = '/etc/zabbix/externalscripts',
+  $purge_conf_dir            = $zabbix20::purge_conf_dir,
 ) inherits zabbix20 {
 
   package { 'zabbix-server':
@@ -27,11 +32,17 @@ class zabbix20::server (
   file { '/etc/zabbix_server.conf':
     ensure  => file,
     owner   => zabbixsrv,
-    group   => zabbix,
+    group   => root,
     mode    => '0400',
     content => template('zabbix20/zabbix_server.conf.erb'),
     require => Package['zabbix-server'],
     notify  => Service['zabbix-server'],
+  }
+  file { '/etc/zabbix/server-conf.d':
+    ensure  => directory,
+    require => Package['zabbix-server'],
+    recurse => $purge_conf_dir,
+    purge   => $purge_conf_dir,
   }
 
   if $dbhost == 'localhost' {
@@ -46,6 +57,13 @@ class zabbix20::server (
         }
       }
       /^mysql/: {
+        require mysql::server
+        mysql::db { $dbname:
+          user     => $dbuser,
+          password => $dbpass,
+          host     => 'localhost',
+          grant    => ['all'],
+        }
       }
     }
   }
