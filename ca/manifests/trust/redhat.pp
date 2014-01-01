2da4affbec6ca265b49e08b65634bdf460b56fe2
Jakov Sosic <jsosic@gmail.com>
2014-01-01 17:36:31 +0100

    code fixing for production on CA module

--- ca/manifests/trust/redhat.pp
+++ ca/manifests/trust/redhat.pp
@@ -7,14 +7,19 @@ class ca::trust::redhat (
 
   if ( $enable ) {
     exec { 'enable_ca_trust':
-      cmd         => '/usr/bin/update-ca-trust enable',
-      onlyif      => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q \'Status: DISABLED.\'',
+      command     => '/usr/bin/update-ca-trust enable',
+      onlyif      => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q \'status: DISABLED.\'',
+      refreshonly => true,
+      before      => Exec['update_ca_trust'],
+    }
+    exec { 'update_ca_trust':
+      command     => '/usr/bin/update-ca-trust extract',
       refreshonly => true,
     }
   } else {
     exec { 'disable_ca_trust':
-      cmd    => '/usr/bin/update-ca-trust disable',
-      onlyif => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q \'Status: ENABLED.\'',
+      command => '/usr/bin/update-ca-trust disable',
+      onlyif  => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q \'Status: ENABLED.\'',
     }
   }
 
