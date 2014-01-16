ef08991f38c8beeceb3d801d423ee4c773f6c23b
Jakov Sosic <jsosic@gmail.com>
2014-01-16 01:10:12 +0100

    ca module refactored

--- ca/manifests/trust/redhat.pp
+++ ca/manifests/trust/redhat.pp
@@ -5,21 +5,26 @@ class ca::trust::redhat (
   $enable = $::ca::trust::enable,
 ) {
 
+  package { 'ca-certificates': ensure => present, }
+
   if ( $enable ) {
     exec { 'enable_ca_trust':
       command     => '/usr/bin/update-ca-trust enable',
       onlyif      => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q "Status: DISABLED."',
       before      => Exec['update_ca_trust'],
       refreshonly => true,
+      require     => Package['ca-certificates'],
     }
     exec { 'update_ca_trust':
       command     => '/usr/bin/update-ca-trust extract',
       refreshonly => true,
+      require     => Package['ca-certificates'],
     }
   } else {
     exec { 'disable_ca_trust':
       command => '/usr/bin/update-ca-trust disable',
       onlyif  => '/usr/bin/update-ca-trust check 2>&1 | /bin/grep -q "Status: ENABLED."',
+      require => Package['ca-certificates'],
     }
   }
 
