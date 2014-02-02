e6c0365bed194706b88d9133cbc7205c8cfcb436
Ivan Rako <ivan.ico.rako@gmail.com>
2014-02-02 17:20:01 +0100

    modified for ocsinventory::agent

--- ocsinventory/manifests/agent.pp
+++ ocsinventory/manifests/agent.pp
@@ -1,19 +1,21 @@
 # Class: ocsinventory::agent
 #
 # This module manages OCS inventory agent
-#
 class ocsinventory::agent (
-  $package_ensure = 'latest',
-  $server = 'localhost',
-  $pause = 0,
-) {
-  package { 'ocsinventory-agent':
-    ensure   => $package_ensure,
+  $package_ensure         = $ocsinventory::params::package_ensure,
+  $server                 = $ocsinventory::params::server,
+  $pause                  = $ocsinventory::params::pause,
+  $package_name           = $ocsinventory::params::package_name,
+  $ocsinventory_conf_path = $ocsinventory::params::ocsinventory_conf_path,
+  $ocsinventory_conf_tmpl = $ocsinventory::params::ocsinventory_conf_tmpl,
+) inherits ocsinventory::params {
+  package { $package_name :
+    ensure  => $package_ensure,
   }
-  file { '/etc/sysconfig/ocsinventory-agent':
+  file { $ocsinventory_conf_path :
     ensure  => present,
-    require => Package['ocsinventory-agent'],
-    content => template('ocsinventory/ocsinventory-agent.erb'),
+    require => Package[$package_name],
+    content => template($ocsinventory_conf_tmpl),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
