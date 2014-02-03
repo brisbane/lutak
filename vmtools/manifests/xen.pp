cba945bdded8143dcd34b40e0d687eb7bb24688d
Ivan Rako <ivan.ico.rako@gmail.com>
2014-02-03 20:23:46 +0100

    vmtools modified for debian

--- vmtools/manifests/xen.pp
+++ vmtools/manifests/xen.pp
@@ -3,6 +3,18 @@
 # Installs the Xen Tools for XenServer from
 # Srce Intern repo
 class vmtools::xen {
-  include ::yum::repo::srce::intern
-  package { [ 'xe-guest-utilities', 'xe-guest-utilities-xenstore' ]: ensure => present, }
+  case $::osfamily {
+    /(RedHat|redhat)/: {
+      include ::yum::repo::srce::intern
+      package { [ 'xe-guest-utilities', 'xe-guest-utilities-xenstore' ]: ensure => present, }
+    }
+    /(Debian|debian|Ubuntu|ubuntu)/: {
+      include ::aptrepo::debian
+      include ::aptrepo::srce
+      package { [ 'xe-guest-utilities' ]: ensure => present, }
+    }
+    default: {
+      fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} currently only supports osfamily RedHat and Debian")
+    }
+  }
 }
