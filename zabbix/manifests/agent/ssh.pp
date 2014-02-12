d7011137021a428e64c09e095a8485ebd067138e
Ivan Rako <ivan.ico.rako@gmail.com>
2014-02-12 12:58:22 +0100

    changed some things for production

--- zabbix/manifests/agent/ssh.pp
+++ zabbix/manifests/agent/ssh.pp
@@ -8,8 +8,20 @@ class zabbix::agent::ssh (
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
 ) inherits zabbix::agent {
 
+  case $::osfamily {
+    default: {
+      $lsof_bin = '/usr/sbin/lsof'
+    }
+    /(RedHat|redhat|amazon)/: {
+      $lsof_bin = '/usr/sbin/lsof'
+    }
+    /(Debian|debian|Ubuntu|ubuntu)/: {
+      $lsof_bin = '/usr/bin/lsof'
+    }
+  }
+
   ::sudoers::allowed_command { 'zabbix_ssh':
-    command          => '/usr/sbin/lsof -i -n -l -P',
+    command          => "${lsof_bin} -i -n -l -P",
     user             => 'zabbix',
     require_password => false,
     comment          => 'Zabbix sensor for monitoring SSH.',
@@ -19,7 +31,7 @@ class zabbix::agent::ssh (
     ensure  => file,
     owner   => root,
     group   => root,
-    source  => 'puppet:///modules/zabbix/agent/ssh.conf',
+    content => template('zabbix/agent/ssh.conf.erb'),
     notify  => Service['zabbix-agent'],
     require => ::Sudoers::Allowed_command['zabbix_ssh'],
   }
