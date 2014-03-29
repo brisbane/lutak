9810e1cbc184c8460202d45232df52c95f391f90
Jakov Sosic <jsosic@gmail.com>
2014-03-30 00:05:18 +0100

    added param 'id' to webmin custom command

--- webmin/manifests/custom_command.pp
+++ webmin/manifests/custom_command.pp
@@ -1,6 +1,7 @@
 # Define: webmin::custom commnad
 define webmin::custom_command (
   $command       = '',
+  $id            = undef,
   $user          = '',
   $description   = '',
   $documentation = '',
@@ -8,7 +9,11 @@ define webmin::custom_command (
 
   # Webmin checks if custom command passes '\d+.cmd'
   # regex, and loads it only if it does
-  $cc_filename = inline_template('<% require \'zlib\'; crc32=Zlib::crc32(title)%><%= crc32 %>')
+  if is_integer($id) {
+    $cc_filename = $id
+  } else {
+    $cc_filename = inline_template('<% require \'zlib\'; crc32=Zlib::crc32(title)%><%= crc32 %>')
+  }
 
   file { "/etc/webmin/custom/${cc_filename}.cmd":
     ensure  => file,
