a7a0dd4b909c73d70dfbc214a40207cafecec9f0
Emir Imamagic <emir.imamagic@gmail.com>
2014-04-01 09:08:21 +0200

    Added RH Developer Toolset repo, correct link in atomic.repo.

---
+++ yum/manifests/repo/devtoolset.pp
@@ -0,0 +1,19 @@
+# Class: yum::repo::devtoolset
+#
+# This module manages CERN devtoolset repo files for $lsbdistrelease
+#
+class yum::repo::devtoolset(
+  $stage    = 'yumsetup',
+  $priority = 1,
+  $exclude  = [],
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/devtoolset.repo' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/devtoolset.erb"),
+  }
+}
