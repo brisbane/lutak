bf950cf87087542b96a59438c4d70e735d674101
Jakov Sosic <jsosic@gmail.com>
2014-05-20 17:34:27 +0200

    [supervisor]: WARN: overwrites config file

--- supervisor/manifests/init.pp
+++ supervisor/manifests/init.pp
@@ -1,8 +1,12 @@
-# Class: supervisor
+#
+# = Class: supervisor
 #
 # This modules installs and manages supervisor
 #
-class supervisor {
+class supervisor (
+  $file_supervisord_conf     = 'puppet:///modules/supervisor/supervisord.conf',
+) inherits supervisor::params {
+
   package { 'supervisor':
     ensure => installed,
   }
@@ -12,4 +16,15 @@ class supervisor {
     enable  => true,
     require => Package['supervisor'],
   }
+
+  file { '/etc/supervisord.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => $file_supervisord_conf,
+    require => Package['supervisor'],
+    notify  => Service['supervisord'],
+  }
+
 }
