963516a835ddfe3989de7c845ec82ce3bd823584
Jakov Sosic <jsosic@gmail.com>
2014-08-26 18:02:01 +0200

    [yum] fixes in vmware-osps and zfs repos

--- yum/manifests/repo/vmware.pp
+++ yum/manifests/repo/vmware.pp
@@ -1,44 +1,29 @@
-# Class: yum::repo::vmware
+#
+# = Class: yum::repo::vmware
 #
 # This module manages vmware repo files for $operatingsystemrelease
 #
 class yum::repo::vmware (
-  $stage   = 'yumsetup',
-  $exclude  = [],
+  $stage        = 'yumsetup',
+  $priority     = '1',
+  $exclude      = [],
+  $esxi_version = '5.5latest',
+  $version      = '9.4.6-1.el6',
 ){
-  require yum::repo::base
+  require ::yum::repo::base
 
-  case $::operatingsystemrelease {
-    default : {}
-    /^5.*/: {
-      file { '/etc/yum.repos.d/vmware-osps.repo':
-        ensure  => file,
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-        source  => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/vmware-osps.repo",
-        require => Package['vmware-tools-repo-RHEL5'],
-      }
-      package { 'vmware-tools-repo-RHEL5':
-        ensure   => '8.6.5-2',
-        provider => 'rpm',
-        source   => "http://packages.vmware.com/tools/esx/5.0u1/repos/vmware-tools-repo-RHEL5-8.6.5-2.${::architecture}.rpm",
-      }
-    }
-    /^6.*/: {
-      file { '/etc/yum.repos.d/vmware-osps.repo':
-        ensure  => file,
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-        source  => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/vmware-osps.repo",
-        require => Package['vmware-tools-repo-RHEL5'],
-      }
-      package { 'vmware-tools-repo-RHEL6':
-        ensure   => '8.6.5-2',
-        provider => 'rpm',
-        source   => "http://packages.vmware.com/tools/esx/5.0u1/repos/vmware-tools-repo-RHEL6-8.6.5-2.${::architecture}.rpm",
-      }
-    }
+  file { '/etc/yum.repos.d/vmware-osps.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/vmware-osps.erb"),
+    require => Package['vmware-tools-repo-RHEL'],
+  }
+  package { 'vmware-tools-repo-RHEL':
+    ensure   => $version,
+    name     => "vmware-tools-repo-RHEL${::operatingsystemmajrelease}",
+    provider => 'rpm',
+    source   => "http://packages.vmware.com/tools/esx/${esxi_version}/repos/vmware-tools-repo-RHEL${::operatingsystemmajrelease}-${version}.${::architecture}.rpm",
   }
 }
