92d5129594e35438546a1ede8d16d80231942097
Jakov Sosic <jsosic@gmail.com>
2014-11-09 14:16:19 +0100

    [nginx] various fixes, added new params

--- nginx/manifests/config.pp
+++ nginx/manifests/config.pp
@@ -1,22 +1,19 @@
-# Class: nginx::config
+#
+# = Class: nginx::config
 #
 # This class provides some advanced nginx settings
 #
 # Parameters:
 #   [*server_names_hash_bucket_size*] - maximum length of a virtual host entry
+#   [*types_hash_max_size*]           - maximum size of the types hash tables
+#   [*types_hash_bucket_size*]        - bucket size for the types hash tables
 #
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#  nginx::config {
-#    server_names_hash_bucket_size => '64',
-#  }
 class nginx::config (
   $server_names_hash_bucket_size = '64',
+  $types_hash_max_size           = '1024',
+  $types_hash_bucket_size        = '64',
 ) {
-  include nginx
+  include ::nginx
 
   $curnotify = $::nginx::manage_service ? {
     default => undef,
