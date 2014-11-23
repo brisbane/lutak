3ee279db831c182b9ef6fe1cd5271dda110f7014
Jakov Sosic <jsosic@gmail.com>
2014-11-23 23:52:56 +0100

    [redmine] passenger repo swapped for epel

--- redmine/manifests/dependency.pp
+++ redmine/manifests/dependency.pp
@@ -4,7 +4,7 @@
 # This class lists dependencies for class redmine
 #
 class redmine::dependency {
-  include ::yum::repo::passenger
+  include ::yum::repo::epel
   include ::yum::repo::srce::test
   include ::ruby::gems
   include ::ruby::gem::rails
