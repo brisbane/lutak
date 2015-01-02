f5db43c8acda0a1e600ca2a0637cf0d57942494a
Jakov Sosic <jsosic@gmail.com>
2015-01-02 16:18:35 +0100

    [webmin] changes to decouple from version dependency

--- webmin/manifests/init.pp
+++ webmin/manifests/init.pp
@@ -6,7 +6,7 @@
 class webmin (
   $ensure        = 'present',
   $package       = 'webmin',
-  $version       = '1.690-1',
+  $version       = latest,
   $libwrap       = '0',
   $alwaysresolve = '1',
   $allow         = [ '0.0.0.0/0' ],
@@ -51,28 +51,28 @@ class webmin (
 
   # webmin.acl
   concat { '/etc/webmin/webmin.acl':
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    notify  => Service['webmin'],
+    owner  => root,
+    group  => root,
+    mode   => '0600',
+    notify => Service['webmin'],
   }
-  concat::fragment { 'webmin_acl:header':
-    target  => '/etc/webmin/webmin.acl',
-    source  => 'puppet:///modules/webmin/webmin.acl',
-    order   => '100',
+  ::concat::fragment { 'webmin_acl:header':
+    target => '/etc/webmin/webmin.acl',
+    source => 'puppet:///modules/webmin/webmin.acl',
+    order  => '100',
   }
 
   # miniserv.users
   concat { '/etc/webmin/miniserv.users':
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    notify  => Service['webmin'],
+    owner  => root,
+    group  => root,
+    mode   => '0600',
+    notify => Service['webmin'],
   }
-  concat::fragment { 'miniserv_users:header':
-    target  => '/etc/webmin/miniserv.users',
-    source  => 'puppet:///modules/webmin/miniserv.users',
-    order   => '100',
+  ::concat::fragment { 'miniserv_users:header':
+    target => '/etc/webmin/miniserv.users',
+    source => 'puppet:///modules/webmin/miniserv.users',
+    order  => '100',
   }
 
 
